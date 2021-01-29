4e5beb9fec25769783d81cca7f61077d788b3075
wrwilliam <williamwruan@gmail.com>
2021-01-29 22:10:25 +0800

    Update javacourse1.java
    adding a class of people

--- javacourse1.java
+++ javacourse1.java
@@ -2,4 +2,8 @@ public static void main(String[]args)
         {
         system.out.println("Hello World!");
         system.out.println("Welcome to Java!");
+        system.out.println(people(mr.zhang));
         }
+class string peole(name)
+{
+}
